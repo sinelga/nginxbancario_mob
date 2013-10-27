25a8abce7c7d90f588a7a74b7466b70eb0d9d4b2
Sinelga <support@sinelga.com>
2013-10-28 00:32:04 +0200

    prod

--- nginxbancario_mob/dartapp/web/dartapp.dart
+++ nginxbancario_mob/dartapp/web/dartapp.dart
@@ -3,62 +3,68 @@ import 'dart:async';
 import 'domains.dart';
 import "package:js/js.dart" as js;
 import "package:jsonp/jsonp.dart" as jsonp;
+import 'events/clickonitemevent.dart' as clickonitemevent;
 
 List<RssFeedItem> rssFeedItemArr;
+List<ForMark> forMarkList;
+var rssfeeder;
+
 void main() {
-//  query("#sample_text_id")
-//    ..text = "Click me!"
-//    ..onClick.listen(reverseText);
   
+  forMarkList = new List<ForMark>();
+    
   Future<js.Proxy> result = jsonp.fetch(
-      uri: "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%20%3D%20%22http%3A%2F%2Fquotidianohome.feedsportal.com%2Fc%2F33327%2Ff%2F565662%2Findex.rss%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=?"
-        
+//      select * from data.html.cssselect where url="www.corriere.it/economia/corriereconomia" and css=".homearticle-box"
+//      uri: "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20data.html.cssselect%20where%20url%3D%22www.corriere.it%2Feconomia%2Fcorriereconomia%22%20and%20css%3D%22.homearticle-box%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=?"
+        uri: "http://146.185.151.26/redis?callback=?"
+  
   );
+  
   result.then((js.Proxy proxy) {
-    rssFeedItemArr = new List<RssFeedItem>();   
-    var items = proxy["query"]["results"]["item"];
-    for (var i=0;i < items.length;i++){
+
+    var items = proxy;
+    
+    for (var i=0;i < 12;i++){
+      ForMark forMark = new ForMark();
       
-      RssFeedItem rssFeedItem =new RssFeedItem();
-      
-      rssFeedItem.title = items[i]["title"];
-      rssFeedItem.link = items[i]["link"];
-      rssFeedItem.pubDate = items[i]["pubDate"];
-      
-      var string = items[i]["description"];
-      var found = string.indexOf('<img') != -1;
-      if (found) {
-      var foundpos = string.indexOf('<img');       
-        rssFeedItem.description = string.substring(0, foundpos);
-      }
-      rssFeedItemArr.add(rssFeedItem);
-//      count++;
+      forMark.Cont = items[i]["Cont"];
+      forMark.ImageLink = items[i]["ImgLink"];
+      forMark.Title = items[i]["Title"];
+      forMarkList.add(forMark);
+
     }
     js.release(proxy);
     
-    var rsstext = query("#rssfeed").text;
-    var bufferrss = new StringBuffer();
-    
-    for (int i =0;i < rssFeedItemArr.length;i++) {
-      bufferrss.write("<div class=\"well\">");
-      bufferrss.writeln(rssFeedItemArr[i].title);
-      bufferrss.write("</div>");
+    for (var i=0;i < forMarkList.length;i++){
+      
+      createMediaObject(i,forMarkList[i]);
       
     }
-
-    HtmlElement htmlelement = query('#rssfeed');
-    htmlelement.innerHtml = bufferrss.toString();
     
   });
   
-  
+  rssfeeder = querySelector("#rssfeeder");
+    
 }
+createMediaObject(i,ForMark item){
+  
+  var id = i.toString();
+  var title ="<i class='fa fa-share fa-2x'></i><div class='googlefonttitle'>"+item.Title+".</div>";
+  var pubdate = item.PubDate;
+  var imagelink = item.ImageLink;
+  var cont = "<p class='media-heading googlefontcont'>"+item.Cont.substring(0, 25)+"...</p>";
+  
+  var htmlstr = "<div class='media'><img class='media-object pull-left itemimage' src='${imagelink}' alt=''><div class='media-body'> <h4 class='media-heading'>${title}</h4>${cont}</div></div>";
+  
+  var divElement = new DivElement();
+  divElement.onClick.listen((event) => clickonitemevent.show(event,forMarkList));
 
-void reverseText(MouseEvent event) {
-  var text = query("#sample_text_id").text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  query("#sample_text_id").text = buffer.toString();
+  divElement.setInnerHtml(htmlstr, treeSanitizer: new NullTreeSanitizer() );
+  divElement.id =id; 
+  
+  rssfeeder.append(divElement);
+ 
+}
+class NullTreeSanitizer implements NodeTreeSanitizer {
+  void sanitizeTree(Node node) {}
 }
--- nginxbancario_mob/dartapp/web/domains.dart
+++ nginxbancario_mob/dartapp/web/domains.dart
@@ -5,3 +5,9 @@ class RssFeedItem {
   String title,link,description,pubDate;
   
 }
+
+class ForMark{
+  
+  String Title,Description,ImageLink,Cont,PubDate;
+ 
+}
